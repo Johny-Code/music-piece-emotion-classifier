4419f0a69a3a51388e31cbda01d085ab411fe27f
michsak <michalsakowicz99@gmail.com>
2023-04-24 01:20:21 +0200

    add CNN architecture training

--- tools/save_melgram_to_file.py
+++ tools/save_melgram_to_file.py
@@ -20,7 +20,7 @@ def save_melgram(filedir, database_filepath, outpath):
             x, sr = librosa.load(filedir+file, sr=44100)
             x = librosa.util.normalize(x)
             x = cut_musical_piece(x, sr, 30)
-            melspectogram = librosa.feature.melspectrogram(y=x, sr=sr, n_mels=96, n_fft=2048, hop_length=512, window=signal.windows.kaiser(M=2048, beta=6))
+            melspectogram = librosa.feature.melspectrogram(y=x, sr=sr, n_mels=128, n_fft=2048, hop_length=1024)
             melgram = librosa.amplitude_to_db(melspectogram, ref=1.0)[np.newaxis,np.newaxis,:,:]
             outfile = outpath + emotion + '/' + name_+'.npy'
             np.save(outfile,melgram)
@@ -29,7 +29,7 @@ def save_melgram(filedir, database_filepath, outpath):
 
 
 if __name__=="__main__":
-    outpath = "../database/melgrams/melgrams_2048_nfft_512_hop_kaiser/"
+    outpath = "../database/melgrams/melgrams_2048_nfft_1024_hop_128_mel/"
     filedir = "../database/songs/"
     database_filepath = "../database/MoodyLyrics4Q.csv"
     save_melgram(filedir, database_filepath, outpath)
