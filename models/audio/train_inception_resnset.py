2f3b83542a192b423d9b8bf5692eb6e547c274fb
michsak <michalsakowicz99@gmail.com>
2023-05-24 07:35:11 +0000

    add proper learning rate scheduler

--- models/audio/train_inception_resnset.py
+++ models/audio/train_inception_resnset.py
@@ -4,7 +4,7 @@ sys.path.append("../../utils/")
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 from train_network import train_val_split, plot_acc_loss
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from keras.regularizers import L2
 from keras.optimizers import Adam
 from keras.applications import ResNet50, ResNet152V2
@@ -38,13 +38,6 @@ def define_inception_resnet_customized_architecture(input_shape, nb_classes):
     return model
 
 
-def scheduler(epoch, lr):
-    if (epoch > 1) and (epoch // 10 == 0):
-        return lr / 2
-    else:
-        return lr
-
-
 if __name__ == "__main__":
     path = "../../database/melgrams/melgrams_2048_nfft_1024_hop_128_mel_jpg/"
     files_nb = 200
@@ -52,15 +45,15 @@ if __name__ == "__main__":
     IMG_WIDTH = 216
     NUM_CLASSES = 4
     NUM_EPOCHS = 100
-    BATCH_SIZE = 64
+    BATCH_SIZE = 32
     L2_LAMBDA = 0.001
     TRAIN_SPLIT = 0.8
-    LEARNING_RATE = 1e-5
+    LEARNING_RATE = 0.01
     STEPS_PER_EPOCH = int(files_nb * TRAIN_SPLIT) // BATCH_SIZE
     VAL_STEPS = int(files_nb * (1 - TRAIN_SPLIT)) // BATCH_SIZE
 
     optimizer = Adam(learning_rate=LEARNING_RATE)
-    learning_rate_callback = LearningRateScheduler(scheduler)
+    reduce_lr_callback = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr=1e-5)
     loss = 'sparse_categorical_crossentropy'
     metrics = ['sparse_categorical_accuracy']
     filepath = "./transfer_learning_epoch_{epoch:02d}_{sparse_categorical_accuracy:.4f}.h5"
@@ -68,12 +61,10 @@ if __name__ == "__main__":
                                  monitor='val_sparse_categorical_accuracy',
                                  verbose=0,
                                  save_best_only=False)
-    callbacks_list = [learning_rate_callback]  # , checkpoint]
+    callbacks_list = [reduce_lr_callback]  # , checkpoint]
 
     model = define_inception_resnet_customized_architecture((IMG_WIDTH, IMG_HEIGHT, 3), 4)
-
     model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-    
     model.build(input_shape=(None, IMG_WIDTH, IMG_HEIGHT, 3))
     print(model.summary())
 
@@ -82,7 +73,7 @@ if __name__ == "__main__":
     history = model.fit(train,
                         epochs=NUM_EPOCHS,
                         # steps_per_epoch=STEPS_PER_EPOCH,
-                        validation_data=test,
+                        validation_data=test,)
                         # validation_steps=VAL_STEPS,)
-                        callbacks=callbacks_list)
+                        # callbacks=callbacks_list)
     plot_acc_loss(history)
