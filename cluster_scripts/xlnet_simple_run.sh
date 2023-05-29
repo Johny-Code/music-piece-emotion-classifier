ffa43d3e2c94ef4d604a91a67ac7b23d0fb09cb5
Johny-Code <jan.tobolewski09@gmail.com>
2023-05-30 01:59:03 +0200

    base form of classificator

---
+++ cluster_scripts/xlnet_simple_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#
+#SBATCH -J xlnet_simple_run
+#SBATCH -o logs/xlnet_simple_run."%j".out
+#SBATCH -e logs/xlnet_simple_run."%j".err
+#
+#SBATCH --mail-user s175502@student.pg.edu.pl
+#SBATCH --mail-type=ALL
+#
+#SBATCH --nodelist=node101 OR node302 OR node108
+#SBATCH -p short
+
+# activate python venv
+source .././emotion_env/bin/activate
+
+# train model
+python3 models/lyric/train_xlnet.py
