d930b1c7083fc9ba6e773cca79cc8f42c68821b1
Johny-Code <jan.tobolewski09@gmail.com>
2023-06-02 21:54:01 +0200

    debuging

--- models/lyric/train_ann.py
+++ models/lyric/train_ann.py
@@ -7,11 +7,67 @@ import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.preprocessing import OneHotEncoder
 from wandb.keras import WandbMetricsLogger
 
-from train_svm import load_data, TARGET_NAMES, SEED
+from train_svm import read_data, TARGET_NAMES, SEED
 # from utils.draw_plot import draw_confusion_matrix, plot_acc_loss
 
+def clean_features(df):
+
+    target_dict = {'happy': 0, 'angry': 1, 'sad': 2, 'relaxed': 3}
+
+    ohe = OneHotEncoder()
+
+    X = []
+    y = []
+
+    for row in df.iterrows():
+
+        emotion = row[1][0]
+        y.append(target_dict[emotion])    
+
+        vector = row[1][1]
+        vector = row[1][1]
+        vector = vector.replace('[', '')
+        vector = vector.replace(']', '')
+        vector = vector.replace('\n', '')
+        vector = vector.replace('   ', ' ')
+        vector = vector.split(' ')
+
+        vector_cleaned = []
+        for value in vector:
+            if value != '':
+                vector_cleaned.append(float(value))
+
+        temp = []
+        for i, ele in enumerate(row[1][2:11]):
+            if i < 10:
+                if ele == 'True':
+                    temp.append(1)
+                elif ele == 'False':
+                    temp.append(0)
+                temp.append(ele)
+
+        X.append(temp)    
+
+    #y has shape (1, n)
+    y_transformed = ohe.fit_transform(np.array(y).reshape(-1, 1))
+    X_transformed = np.array(X)
+
+    return X_transformed, y_transformed
+
+def load_data():
+
+    input_path = os.path.join('database', 'features', 'lyric_features.csv')
+    df = read_data(input_path)
+
+    X, y = clean_features(df)
+
+    print(f"input data shape: {X.shape}")
+    print(f"output data shape: {y.shape}")
+
+    exit(0)
 
 def build_4_dense_ann(input_size=309, dense_size=128, output_size=4, activation='relu', dropout=0.2, optimizer='adam'):
 
@@ -36,7 +92,7 @@ def train_ann(X_train, y_train, X_test, y_test, params):
 
     print(f"input data shape: {X_train.shape}")
     print(f"output data shape: {y_train.shape}")
-    
+
     X_test, y_test, X_val, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=SEED)
 
     input_size = 309
