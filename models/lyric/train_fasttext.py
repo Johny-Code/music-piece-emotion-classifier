822156ae36a339f0fc545ba21b46ec34aa881a7c
Johny-Code <jan.tobolewski09@gmail.com>
2023-06-02 20:28:06 +0200

    change auto tune time

--- models/lyric/train_fasttext.py
+++ models/lyric/train_fasttext.py
@@ -215,7 +215,7 @@ if __name__ == '__main__':
                        'loss': 'ova',
                        'thread': 4,
                        'replace_newline': ' ',
-                        'autotune_duration': 600
+                        'autotune_duration': 1800
                        }
 
         simple_run(hyperparams)
