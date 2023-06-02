e96c05482639af7899685321865b7a4515e3c709
Johny-Code <jan.tobolewski09@gmail.com>
2023-06-02 20:56:33 +0200

    wandb logs

--- models/lyric/train_ann.py
+++ models/lyric/train_ann.py
@@ -2,16 +2,18 @@ import sys
 import argparse
 import keras
 import os
+import wandb
 import numpy as np
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report
+from wandb.keras import WandbMetricsLogger
 
 from train_svm import read_data, TARGET_NAMES, SEED
 from utils.draw_plot import draw_confusion_matrix, plot_acc_loss
 
 
-def build_4_dense_ann(input_size=321, dense_size=128, output_size=4, activation='relu', dropout=0.2, optimizer='adam'):
+def build_4_dense_ann(input_size=309, dense_size=128, output_size=4, activation='relu', dropout=0.2, optimizer='adam'):
 
     model = keras.models.Sequential(
         keras.layers.Dense(dense_size, input_dim=input_size, activation=activation),
@@ -34,25 +36,19 @@ def train_ann(X_train, y_train, X_test, y_test, params):
 
     X_test, y_test, X_val, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=SEED)
 
-    params = {
-        'lr': 0.01,
-        'epochs': 100,
-        'batch_size': 32,
-        'dense_size': 128,
-        'activation': 'relu',
-        'dropout': 0.2,
-        'optimizer': 'adam'
-    }
-
     input_size = len(X_train[0])
     output_size = len(TARGET_NAMES)
 
     model = build_4_dense_ann(input_size, params['dense_size'], output_size, params['activation'], params['dropout'], params['optimizer'])
 
-    history = model.fit(X_train, y_train, epochs=params['epochs'], batch_size=params['batch_size'], validation_data=(X_val, y_val))
+    history = model.fit(X_train, y_train, 
+                        epochs=params['epochs'], 
+                        batch_size=params['batch_size'], 
+                        validation_data=(X_val, y_val),
+                        callbacks=[WandbMetricsLogger()])
 
-    acc_loss_out_path = os.path.join('models', 'lyric', 'ann_acc_loss.png')
-    plot_acc_loss(history, acc_loss_out_path)
+    # acc_loss_out_path = os.path.join('models', 'lyric', 'ann_acc_loss.png')
+    # plot_acc_loss(history, acc_loss_out_path)
 
     score = model.evaluate(X_test, y_test, batch_size=params['batch_size'])
 
@@ -65,8 +61,12 @@ def train_ann(X_train, y_train, X_test, y_test, params):
     cm = confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1))
     print(cm)
 
-    output_path = os.path.join('models', 'lyric', 'history', 'ann')
-    draw_confusion_matrix(cm, TARGET_NAMES, )
+    print(classification_report(y_test.argmax(axis=1), y_pred.argmax(axis=1), target_names=TARGET_NAMES))
+
+    wandb.log(f"Classification report {classification_report(y_test.argmax(axis=1), y_pred.argmax(axis=1), target_names=TARGET_NAMES)}")
+
+    # output_path = os.path.join('models', 'lyric', 'history', 'ann')
+    # draw_confusion_matrix(cm, TARGET_NAMES, )
 
 
 def grid_search_ann():
@@ -81,15 +81,23 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.simple_run:
+
+        run = wandb.init(project='feature-based-ann',
+                   
+                   config={'lr': 0.02,
+                           'epochs': 100,
+                           'batch_size': 32,
+                           'dense_size': 128,
+                           'activation': 'relu',
+                           'dropout': 0.2,
+                           'optimizer': 'adam'
+                           }
+                    
+                )
+                    
         X_train, X_test, y_train, y_test = read_data()
 
-        params = {
-            'lr': 0.01,
-            'epochs': 100,
-            'batch_size': 32,
-        }
-
-        train_ann(X_train, y_train, X_test, y_test, params)
+        train_ann(X_train, y_train, X_test, y_test, run.config)
 
     elif args.grid_search:
         pass
