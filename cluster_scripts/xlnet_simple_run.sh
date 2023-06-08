c78c548a34c8e409a395f9db2e184a04694dd1b4
Johny-Code <jan.tobolewski09@gmail.com>
2023-06-08 09:07:28 +0200

    memory reservation

--- cluster_scripts/xlnet_simple_run.sh
+++ cluster_scripts/xlnet_simple_run.sh
@@ -7,6 +7,7 @@
 #SBATCH --mail-user s175502@student.pg.edu.pl
 #SBATCH --mail-type=ALL
 #
+#SBATCH --mem=10G
 #SBATCH --nodelist=node101
 #SBATCH -p short
 
--- models/lyric/train_xlnet.py
+++ models/lyric/train_xlnet.py
@@ -5,6 +5,7 @@ import torch
 import time
 import datetime
 import math
+# import logging
 
 import numpy as np
 import pandas as pd
@@ -16,8 +17,11 @@ from torch.utils.data import TensorDataset, DataLoader, RandomSampler, Sequentia
 sys.path.append('tools/')
 from extract_features_from_lyric import load_en_dataset, clean_lyric
 
+
 SEED = 100
 torch.manual_seed(SEED)
+
+TARGET_NAMES = ['happy', 'angry', 'sad', 'relaxed']
 
 def preprocess(dataset, remove_newline):
 
@@ -274,14 +278,14 @@ def train(model, optimizer, train_dataloader, validation_dataloader, hyperparame
         if lowest_eval_loss == None:
             lowest_eval_loss = epoch_eval_loss
             print(f'Best performance achived at epoch {actual_epoch} with validation loss of {lowest_eval_loss}')
-            save_model(model, hyperparameters['model_save_path'],
-                       actual_epoch, lowest_eval_loss, train_loss_set, valid_loss_set)
+            # save_model(model, hyperparameters['model_save_path'],
+                    #    actual_epoch, lowest_eval_loss, train_loss_set, valid_loss_set)
         else:
             if epoch_eval_loss < lowest_eval_loss:
                 lowest_eval_loss = epoch_eval_loss
                 print(f'At epoch {actual_epoch} better performance was achived with validation loss of {lowest_eval_loss}')
-                save_model(model, hyperparameters['model_save_path'],
-                           actual_epoch, lowest_eval_loss, train_loss_set, valid_loss_set)
+                # save_model(model, hyperparameters['model_save_path'],
+                        #    actual_epoch, lowest_eval_loss, train_loss_set, valid_loss_set)
     
     print("")
     print("Training complete!")
@@ -320,7 +324,9 @@ def test_model(model,test_dataloader):
     print("Testing complete!")
     print("")
     print("Classification Report:")
-    print(classification_report(y_true, y_pred, digits=3))
+    print(classification_report(y_true, y_pred, target_names=TARGET_NAMES, digits=3))
+
+    # logging.info(classification_report(y_true, y_pred, target_names=TARGET_NAMES, digits=3))
 
 def simple_run(hyperparemeters):
 
@@ -378,6 +384,8 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
 
+    # logging.basicConfig(filename='models/lyric/logs/metrics.log', level=logging.INFO)
+
     if args.fine_tune:
         
         #actual date and time
@@ -386,16 +394,16 @@ if __name__ == '__main__':
 
         hyperparameters = {
                             'tokenizer':{
-                                'do_lower_case': False,
+                                'do_lower_case': True,
                                 'num_embeddings': 256,
                             },
                             'model':{
                                 'num_labels': 4,
-                                'batch_size': 32,
-                                'lr': 2e-5,
+                                'batch_size': 64,
+                                'lr': 5e-6,
                                 'weight_decay': 0.01,
                                 'correct_bias': False,
-                                'epochs': 5,
+                                'epochs': 6,
                             },
                             'model_save_path': model_save_folder
                         }
@@ -450,28 +458,38 @@ if __name__ == '__main__':
     
     elif args.grid_search:
 
-        do_lower_case = [True, False]
-        num_embeddings = [256, 512]
-        
+        # logging.info('Start grid search')
 
+        do_lower_case = [True, False]
+        num_embeddings = [32, 64, 128, 256]
+        batch_sizes = [8, 16, 32, 64, 128]
+        
+        iteration = 0
         for lower_case in do_lower_case:
             for num_embedding in num_embeddings:
+                for batch_size in batch_sizes:  
 
-                hyperparameters = {
-                                    'tokenizer':{
-                                        'do_lower_case': lower_case,
-                                        'num_embeddings': num_embedding,
-                                    },
-                                    'model':{
-                                        'num_labels': 4,
-                                        'batch_size': 32,
-                                        'lr': 2e-5,
-                                        'weight_decay': 0.01,
-                                        'correct_bias': False,
-                                        'epochs': 3,
-                                    },
-                                }
+                    print(iteration)
+                    iteration += 1
+                    hyperparameters = {
+                                        'tokenizer':{
+                                            'do_lower_case': lower_case,
+                                            'num_embeddings': num_embedding,
+                                        },
+                                        'model':{
+                                            'num_labels': 4,
+                                            'batch_size': batch_size,
+                                            'lr': 2e-5,
+                                            'weight_decay': 0.01,
+                                            'correct_bias': False,
+                                            'epochs': 4,
+                                        },
+                                    }
 
-                simple_run(hyperparameters)
+                    simple_run(hyperparameters)
+
+                    for key, value in hyperparameters.items():
+                        print(f"{key} : {value}")
+                        logging.info(f"{key} : {value}")
            
     
