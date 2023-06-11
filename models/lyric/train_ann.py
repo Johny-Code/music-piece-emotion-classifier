73921a06280afb2c8f96d2328c7b0b17c41796a3
Johny-Code <jan.tobolewski09@gmail.com>
2023-06-11 12:46:07 +0200

    metric print

--- models/lyric/train_ann.py
+++ models/lyric/train_ann.py
@@ -127,11 +127,11 @@ def train_ann(X_train, y_train, X_test, y_test, params):
     print(f"Prediction time: {end - start} seconds")
 
     cm = confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1))
-    # print(cm)
+    print(cm)
     wandb.log({"conf_mat": cm})
 
     report = classification_report(y_test.argmax(axis=1), y_pred.argmax(axis=1), target_names=TARGET_NAMES, output_dict=True)
-    # print(report)
+    print(report)
 
     wandb.log({"raw_report": report})
 
