9237eec0c3d7b58db6649d7110a13db356c0d62d
michsak <michalsakowicz99@gmail.com>
2023-08-27 17:32:48 +0200

    add different melgrams size

--- utils/cut_musical_piece.py
+++ utils/cut_musical_piece.py
@@ -1,5 +1,10 @@
-def cut_musical_piece(x, sr, time):
-    middle = int(x.shape[0] / 2)
-    lower = int(middle - sr * time / 2)
-    upper = int(middle + sr * time / 2)
-    return x[lower:upper]
+def cut_musical_piece(x, sr, time, division="middle"):
+    if (division == "middle"):
+        middle = int(x.shape[0] / 2)
+        lower = int(middle - sr * time / 2)
+        upper = int(middle + sr * time / 2)
+        return x[lower:upper]
+    elif(division == "beginning"):
+        return x[0:int(sr * time)]
+    elif(division == "end"):
+        return x[-int(sr * time):]
