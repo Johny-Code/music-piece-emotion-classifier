ccf85da85479ed6ab9f9652aa8e3055fcc436711
michsak <michalsakowicz99@gmail.com>
2023-08-30 09:16:55 +0000

    add validation accuracy to the output file

--- models/audio/train_sarkar_torch.py
+++ models/audio/train_sarkar_torch.py
@@ -16,34 +16,33 @@ from torchsummary import summary
 class SarkarVGGCustomizedArchitecture(nn.Module):
     def __init__(self, num_classes, channels):
         super(SarkarVGGCustomizedArchitecture, self).__init__()
-        
         self.conv1 = nn.Conv2d(channels, 64, kernel_size=3, stride=1, padding=1)
         self.relu1 = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         self.relu2 = nn.ReLU(inplace=True)
         self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        
+
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         self.relu3 = nn.ReLU(inplace=True)
         self.conv4 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         self.relu4 = nn.ReLU(inplace=True)
         self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        
+
         self.conv5 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
         self.relu5 = nn.ReLU(inplace=True)
         self.maxpool3 = nn.MaxPool2d(kernel_size=3, stride=3)
         self.dropout1 = nn.Dropout(0.25)
-        
+
         self.conv6 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
         self.relu6 = nn.ReLU(inplace=True)
         self.maxpool4 = nn.MaxPool2d(kernel_size=3, stride=3)
         self.dropout2 = nn.Dropout(0.25)
-        
+
         self.conv7 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
         self.relu7 = nn.ReLU(inplace=True)
         self.maxpool5 = nn.MaxPool2d(kernel_size=3, stride=3)
         self.dropout3 = nn.Dropout(0.25)
-        
+
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(2816, 256)
         self.relu8 = nn.ReLU()
@@ -53,35 +52,35 @@ class SarkarVGGCustomizedArchitecture(nn.Module):
         self.dropout5 = nn.Dropout(0.5)
         self.fc3 = nn.Linear(256, num_classes)
         self.softmax = nn.Softmax(dim=1)
-
+        
     def forward(self, x):
         x = self.conv1(x)
         x = self.relu1(x)
         x = self.conv2(x)
         x = self.relu2(x)
         x = self.maxpool1(x)
-        
+
         x = self.conv3(x)
         x = self.relu3(x)
         x = self.conv4(x)
         x = self.relu4(x)
         x = self.maxpool2(x)
-        
+
         x = self.conv5(x)
         x = self.relu5(x)
         x = self.maxpool3(x)
         x = self.dropout1(x)
-        
+
         x = self.conv6(x)
         x = self.relu6(x)
         x = self.maxpool4(x)
         x = self.dropout2(x)
-        
+
         x = self.conv7(x)
         x = self.relu7(x)
         x = self.maxpool5(x)
         x = self.dropout3(x)
-        
+
         x = self.flatten(x)
         x = self.fc1(x)
         x = self.relu8(x)
@@ -91,9 +90,9 @@ class SarkarVGGCustomizedArchitecture(nn.Module):
         x = self.dropout5(x)
         x = self.fc3(x)
         x = self.softmax(x)
-        
-        return x
 
+        return x
+    
 
 def save_checkpoint(model, path, current_accuracy, epoch):
     if current_accuracy > 56.: 
